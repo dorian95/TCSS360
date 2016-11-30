ac9423002a00f89960db82959ec8d43b43c7e929
Ryan Potter <rjp24@uw.edu>
2016-11-29 22:46:11 -0800

    merging

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var express = require('express')
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
   , template2 = require('jade').compileFile(__dirname + '/source/templates/main_page.jade')
   , template3 = require('jade').compileFile(__dirname + '/source/templates/add_page.jade')
+  , template4 = require('jade').compileFile(__dirname + '/source/templates/edit_page.jade')
+  , template5 = require('jade').compileFile(__dirname + '/source/templates/delete_page.jade')
+  , template6 = require('jade').compileFile(__dirname + '/source/templates/student_report_page.jade')
+  , template7 = require('jade').compileFile(__dirname + '/source/templates/data_report_page.jade')
+  , template8 = require('jade').compileFile(__dirname + '/source/templates/lookup_student_page.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -42,6 +47,50 @@ app.get('/add_student', function (req, res, next) {
   }
 })
 
+app.get('/edit_student', function (req, res, next) {
+  try {
+    var html = template4({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/delete_student', function (req, res, next) {
+  try {
+    var html = template5({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/student_report', function (req, res, next) {
+  try {
+    var html = template6({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/data_report', function (req, res, next) {
+  try {
+    var html = template7({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/lookup_student_report', function (req, res, next) {
+  try {
+    var html = template8({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
