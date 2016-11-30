8106aaaa5fa1f2b182fc8eceb2fa246dc1a763ae
Nursultan <dorian95@uw.edu>
2016-11-30 10:21:07 -0800

    moved classes from the main dir

---
+++ source/JavaScriptFiles/StaffCollection.js
@@ -0,0 +1,14 @@
+/**
+	A class that represents StaffCollection
+	
+	@author Nursultan Irgaliyev
+	@date 11/30/2016
+**/
+
+	StaffCollection = function() {
+	
+	}
+	
+	function add(Staff) {
+	// if added succesfully - return true
+	}
